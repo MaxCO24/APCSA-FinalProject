56aedea250168a0f335a254edb95e5b58b7f0c2b
800023196 <124196352+800023196@users.noreply.github.com>
2023-05-09 09:49:57 -0700

    Update ChessLabel.java
    consistent syntax

--- ChessBot/CHeSs/ChessLabel.java
+++ ChessBot/CHeSs/ChessLabel.java
@@ -6,21 +6,19 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 public class ChessLabel extends JLabel {
 
-    Font font     = new Font("Ariel", Font.PLAIN, 72);
+    Font font = new Font("Ariel", Font.PLAIN, 72);
     Color bgLight = new Color(222, 184, 135);
-    Color bgDark  = new Color(139, 69, 19);
+    Color bgDark = new Color(139, 69, 19);
 
-    ChessLabel(String s)
-    {
+    ChessLabel(String s) {
         super(s);
     }
 
-    void set(int idx, int row)
-    {
+    void set(int idx, int row) {
       setFont(font);
           setOpaque(true);
           setBackground((idx+row)%2 == 0 ? bgDark : bgLight);
-          setHorizontalAlignment( SwingConstants.CENTER );
+          setHorizontalAlignment(SwingConstants.CENTER);
     }
 
 }
