37c50405d074fc8f1c768cf6c46f4159d341898e
800023196 <124196352+800023196@users.noreply.github.com>
2023-05-09 09:48:56 -0700

    Update Board.java
    more fixes, missed some previously

--- ChessBot/CHeSs/Board.java
+++ ChessBot/CHeSs/Board.java
@@ -16,36 +16,36 @@ public class Board extends JFrame {
             // black
             new ChessLabel("\u265C"), new ChessLabel("\u265E"), new ChessLabel("\u265D"), 
             new ChessLabel("\u265B"), new ChessLabel("\u265A"), new ChessLabel("\u265D"), 
-            new ChessLabel("\u265E"), new ChessLabel("\u265C")]
+            new ChessLabel("\u265E"), new ChessLabel("\u265C"),
 
             new ChessLabel("\u265F"), new ChessLabel("\u265F"), new ChessLabel("\u265F"), 
             new ChessLabel("\u265F"), new ChessLabel("\u265F"), new ChessLabel("\u265F"), 
-            new ChessLabel("\u265F"), new ChessLabel("\u265F")], 
+            new ChessLabel("\u265F"), new ChessLabel("\u265F"), 
             
             // empty
             new ChessLabel(" "), new ChessLabel(" "), new ChessLabel(" "), 
             new ChessLabel(" "), new ChessLabel(" "), new ChessLabel(" "), 
-            new ChessLabel(" "), new ChessLabel(" ")], 
+            new ChessLabel(" "), new ChessLabel(" "), 
             
             new ChessLabel(" "), new ChessLabel(" "), new ChessLabel(" "), 
             new ChessLabel(" "), new ChessLabel(" "), new ChessLabel(" "), 
-            new ChessLabel(" "), new ChessLabel(" ")], 
+            new ChessLabel(" "), new ChessLabel(" "), 
 
             new ChessLabel(" "), new ChessLabel(" "), new ChessLabel(" "), 
             new ChessLabel(" "), new ChessLabel(" "), new ChessLabel(" "), 
-            new ChessLabel(" "), new ChessLabel(" ")],
+            new ChessLabel(" "), new ChessLabel(" "),
 
             new ChessLabel(" "), new ChessLabel(" "), new ChessLabel(" "), 
             new ChessLabel(" "), new ChessLabel(" "), new ChessLabel(" "), 
-            new ChessLabel(" "), new ChessLabel(" ")],
+            new ChessLabel(" "), new ChessLabel(" "),
             // white
             new ChessLabel("\u2659"), new ChessLabel("\u2659"), new ChessLabel("\u2659"), 
             new ChessLabel("\u2659"), new ChessLabel("\u2659"), new ChessLabel("\u2659"), 
-            new ChessLabel("\u2659"), new ChessLabel("\u2659")],
+            new ChessLabel("\u2659"), new ChessLabel("\u2659"),
             
             new ChessLabel("\u2656"), new ChessLabel("\u2658"), new ChessLabel("\u2657"), 
             new ChessLabel("\u2655"), new ChessLabel("\u2654"), new ChessLabel("\u2657"), 
-            new ChessLabel("\u2658"), new ChessLabel("\u2656")]
+            new ChessLabel("\u2658"), new ChessLabel("\u2656")
         };
     } // Board()
 
