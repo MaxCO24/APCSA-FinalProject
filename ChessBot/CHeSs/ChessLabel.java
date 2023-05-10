096ff4e016d38b8fee62bf4c7a83d1c575ae3a24
sykadelix <126105182+sykadelix@users.noreply.github.com>
2023-05-09 21:15:16 -0700

    Switched from unicode to PNG pieces!

--- ChessBot/CHeSs/ChessLabel.java
+++ ChessBot/CHeSs/ChessLabel.java
@@ -3,21 +3,27 @@ package CHeSs;
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.ImageIcon;
 
 public class ChessLabel extends JLabel implements MouseListener, MouseMotionListener {
 
     Font font = new Font("Ariel", Font.PLAIN, 72);
     Color bgLight = new Color(222, 184, 135);
     Color bgDark = new Color(139, 69, 19);
-
-    ChessLabel(String s) {
-        super(s);
-    }
+    
+    public ChessLabel(String imageName)
+    {
+        String folder = "chessPieces";
+        ImageIcon icon = new ImageIcon(folder + "\\" + imageName);
+        setIcon(icon);
+        setHorizontalAlignment(SwingConstants.CENTER);
+        setVerticalAlignment(SwingConstants.CENTER); 
+    } // fancy new constructor! -ethan
 
     void set(int idx, int row) {
       setFont(font);
           setOpaque(true);
-          setBackground((idx+row)%2 == 0 ? bgDark : bgLight);
+          setBackground((idx+row)%2 == 0 ? bgLight : bgDark);
           setHorizontalAlignment(SwingConstants.CENTER);
     }
 
