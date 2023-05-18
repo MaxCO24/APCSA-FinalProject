7e46ccf2564af14d5ddb4fd37f30979c7ed8269d
sykadelix <126105182+sykadelix@users.noreply.github.com>
2023-05-18 00:15:07 -0700

    improved constructor

--- ChessBot/CHeSs/ChessLabel.java
+++ ChessBot/CHeSs/ChessLabel.java
@@ -7,15 +7,77 @@ public class ChessLabel extends JLabel {
     Font f = new Font("Ariel", Font.PLAIN, 90);
     Color bgLight = new Color(222, 184, 135);
     Color bgDark = new Color(139, 69, 19);
+    private String pieceType;
+    private String pieceColor;
+    
+    
+    ChessLabel(String s, String w) { //modified constructor; takes piece type and bool determines color (true will always mean white and vice versa)
+        super(ChessLabel.strToUni(s, w));
+        
+        pieceType = s;
+        pieceColor = w;
+        
+    }
+    
+    ChessLabel() //constructor for empty spaces
+    {
+        super(" ");
+        pieceType = " ";
+        pieceColor = " ";
+    }
 
-    ChessLabel(String s) {
-        super(s);
+    public String getPieceType() {return pieceType;}
+    public String getPieceColor() {return pieceColor;}
+    public void setPieceType(String pt) {pieceType = pt;}
+    public boolean isEmpty() { return pieceType.equals(" ");}
+    public boolean isWhite() { return pieceColor.equals("w");}
+    public boolean isBlack() { return pieceColor.equals("b");}
+    
+
+    public static String strToUni(String piece, String color) //converts readable piece name string into unicode char for display()
+    {
+        if(color.equals("w"))
+        {
+            switch (piece)
+            {
+                case "p":
+                    return "\u2659";
+                case "r":
+                    return "\u2656";
+                case "kn":
+                    return "\u2658";
+                case "b":
+                    return "\u2657";
+                case "q":
+                    return "\u2655";
+                case "k":
+                    return "\u2654";
+            }
+        } else {
+            switch (piece)
+            {
+                case "p":
+                    return "\u265F";
+                case "r":
+                    return "\u265C";
+                case "kn":
+                    return "\u265E";
+                case "b":
+                    return "\u265D";
+                case "q":
+                    return "\u265B";
+                case "k":
+                    return "\u265A";
+            }
+
+        }
+        return " ";
     }
 
     void set(int idx, int row) {
         setFont(f);
         setOpaque(true);
-        setBackground((idx+row)%2 == 0 ? bgDark : bgLight);
+        setBackground((idx+row)%2 == 0 ? bgLight : bgDark);
         setHorizontalAlignment(SwingConstants.CENTER);
     }
 }
